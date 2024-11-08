a543633356146ebcab657efbfe7b8dc53b562e50
Hengrui Jia <hari20020511@gmail.com>
2024-11-07 20:58:36 -0500

    Login Form

--- src/Components/LoginComponents.tsx
+++ src/Components/LoginComponents.tsx
@@ -1,7 +1,28 @@
+import { Container, Row, Col, Form } from "react-bootstrap"
+
 export const LoginComponent: React.FC = () => {
     return (
-        <div>
+        <Container>
 
-        </div>
+            <h3 >Login</h3>
+            <Form>
+                <Form.Group controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        type="username"
+                        placeholder="Enter Username">
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter Password">
+                    </Form.Control>
+                </Form.Group>
+            </Form>
+
+        </Container>
     )
 }
